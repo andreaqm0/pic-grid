cf7c8aa3248f9ed97d55963fa98f23b303345924
Andrea <0318121702@ut-tijuana.edu.mx>
2024-09-26 15:59:41 -0700

    update models.db.ts

--- src/db/models.db.ts
+++ src/db/models.db.ts
@@ -1,3 +1,7 @@
+import { Collection } from 'src/collections/entities/collection.entity';
+import { Comment } from 'src/comments/entities/comment.entity';
+import { Picture } from 'src/pictures/entities/picture.entity';
 import { User } from 'src/users/entities/user.entity';
+import { PictureCollection } from './many-to-many/picture-collection.table';
 
-export const Models = [User];
+export const Models = [User, Picture, Collection, Comment, PictureCollection];
